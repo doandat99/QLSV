2bd9c3e7655d67d64a59dd7e024e3936019a548e
doandat99 <dsky5463@gmail.com>
2022-07-05 14:22:20 +0700

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import Main from "./componentStudent/Main";
-import MainTeacher from "./componentTeacher/MainTeacher";
-import MainClass from "./componentClass/MainClass";
-import Student from "./StudentClass/Student";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Route path="/danh-sach-sinh-vien" exact component={Main} />
-          <Route path="/danh-sach-giao-vien" exact component={MainTeacher} />
-          <Route path="/danh-sach-lop" exact component={MainClass} />
-          <Route path="/quan-ly-sinh-vien-lop" exact component={Student} />
-        </div>
-      </Router>
+      <div className="App">
+        <Main />
+      </div>
     );
   }
 }
